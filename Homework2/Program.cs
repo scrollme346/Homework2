25f32220e44c40018c6c05aca2f901d4c9477a68
scrollme346 <133518123+scrollme346@users.noreply.github.com>
2023-10-29 11:56:54 +0200

    Program.cs

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число");
+        string? oneN = Console.ReadLine();
+        Console.WriteLine("Введите второе число");
+        string? twoN = Console.ReadLine();
+        Console.WriteLine("Введите третье число");
+        string? threeN = Console.ReadLine();
+        Console.WriteLine("Введите четвертое число");
+        string? fourN = Console.ReadLine();
+
+        Console.WriteLine("Ваши числа");
+        Console.WriteLine(oneN + twoN + threeN + fourN);
+    }
+}
