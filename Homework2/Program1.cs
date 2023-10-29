290643736322020c523fb2ce038fbc465712659d
scrollme346 <133518123+scrollme346@users.noreply.github.com>
2023-10-29 12:29:56 +0200

    Program1.cs

---
+++ Homework2/Program1.cs
@@ -0,0 +1,28 @@
+using System.Numerics;
+using System.Transactions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 6-digit number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int num0 = number / 100000;
+
+        int num1 = number / 10000 % 10;
+
+        int num2 = number % 10000 / 1000;
+
+        int num3 = number % 1000 / 100;
+
+        int num4 = number % 100 / 10;
+
+        int num5 = number % 10;
+
+        Console.WriteLine("Your number's backwards");
+        Console.WriteLine($"{num5}{num4}{num3}{num2}{num1}{num0}");
+        
+
+    }
+}
